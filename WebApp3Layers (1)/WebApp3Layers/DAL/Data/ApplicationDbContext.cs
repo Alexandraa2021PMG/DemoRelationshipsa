c8b563ed9775a30ac864db0a3d988a3eb0d33453
Alexandraa2021PMG <a.todorova2021@pmg-pernik.com>
2025-10-23 13:50:55 +0300

    a

---
+++ WebApp3Layers (1)/WebApp3Layers/DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,51 @@
+﻿using DAL.Data.Entities;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Biography> Biographies { get; set; }
+        public virtual DbSet<Company> Companies { get; set; }
+        public virtual DbSet<Employee> Employees { get; set; }
+        public virtual DbSet<Book> Books { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<BookCategory> BooksCategories { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Author>()
+                .HasOne(a => a.Biography)
+                .WithOne(b => b.Author)
+                .HasForeignKey<Biography>(b => b.AthorId);
+
+            builder.Entity<Company>()
+                .HasMany(c => c.Employees)
+                .WithOne(e => e.Company)
+                .HasForeignKey(e => e.CompanyId);
+
+            builder.Entity<BookCategory>()
+                .HasKey(bc => new { bc.BookId, bc.CategoryId });
+
+            builder.Entity<BookCategory>()
+                .HasOne(bc => bc.Book)
+                .WithMany(bc => bc.BooksCategories)
+                .HasForeignKey(bc => bc.BookId);
+
+            builder.Entity<BookCategory>()
+                .HasOne(bc => bc.Category)
+                .WithMany(bc => bc.BooksCategories)
+                .HasForeignKey(bc => bc.CategoryId);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ WebApp3Layers (1)/WebApp3Layers/DAL/Data/Entities/Biography.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Data.Entities
+{
+    public class Biography:BaseEntity
+    {
+        public string BiographyData { get; set; } = null!;
+
+        public DateTime DateOfBirthday { get; set; }
+
+        public string PlaceOfBirth { get; set; } = null!;
+
+        public string Nationality { get; set; } = null!;
+
+        [ForeignKey("Author")]
+        public int AthorId { get; set; }
+
+        public virtual Author Author { get; set; } = null!;
+    }
+}
